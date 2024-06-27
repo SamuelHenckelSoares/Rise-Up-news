53e12869d6d8393ff6a5cc440e433d26755054ce
Samuel H. Soares <samucaelsoares@gmail.com>
2024-06-27 20:39:02 +0000

    make changes on Database catch errors

--- infra/database.js
+++ infra/database.js
@@ -8,18 +8,27 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credênciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   /*const result = await client.query(queryObject);
   await client.end();
   console.log("client has disconnected");
   return result;
-*/
+  */
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.error(err);
+    throw err;
   } finally {
     await client.end();
     console.log("client has disconnected");

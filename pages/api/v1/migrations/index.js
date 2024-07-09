ed2f27f948e3691da01ab03ab53539c474ca124c
Samuel H. Soares <samucaelsoares@gmail.com>
2024-07-09 20:37:36 +0000

    Ajustes com as migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,12 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    //databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -12,9 +15,10 @@ export default async function migrations(request, response) {
   };
 
   if (request.method === "GET") {
-    console.log(request.method);
+    //console.log(request.method);
 
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
     response.status(200).json(pendingMigrations);
   }
 
@@ -24,6 +28,9 @@ export default async function migrations(request, response) {
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
+
     if (migratedMigrations.length > 0) {
       response.status(201).json(migratedMigrations);
     }
